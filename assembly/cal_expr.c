057e3694bac4162ee47c846c03c7d4dd17c0081b
Batwan01 <wldhks1120@naver.com>
2023-11-13 23:26:17 +0900

    cal_expr

---
+++ assembly/cal_expr.c
@@ -0,0 +1,18 @@
+int cal_one_expr(char* expr) {						// ex) cal_one_expr("BUF")
+													// ex) cal_one_expr("VAR3+4")
+	int expv, v1, v2, sign;							// ex) cal_one_expr("10h+10")
+	char* pp = NULL, * mp = NULL, * v2p = NULL;		// pp: '+', mp: '-'
+	if ((sign = issign(*expr)) != 0) expr++;
+	else sign = 1;
+
+	if (pp = strchr(expr, '+')) *pp = 0;
+	else if (mp = strchr(expr, '-')) *mp = 0;
+	if (pp) v2p = pp + 1;
+	else if (mp) v2p = mp + 1;
+	v1 = sign * cnv_one_term(expr);
+	if (v2p) v2 = cnv_one_term(v2p);
+	if (pp) expv = v1 + v2;
+	else if (mp) expv = v1 - v2;
+	else expv = v1;
+	return(expv);
+}
