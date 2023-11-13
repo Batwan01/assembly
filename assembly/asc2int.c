195b20205badf967e596a8566363d87d73e85ea4
Batwan01 <wldhks1120@naver.com>
2023-11-13 23:43:57 +0900

    ass2int

---
+++ assembly/asc2int.c
@@ -0,0 +1,9 @@
+int asc2int(char* ap)			// ex) str2int("AB")
+{
+	int l = strlen(ap), v;
+	if (ap[l - 1] != '\'')
+		return(INV_VAL);
+	for (v = 0; *(ap + 1) != '\''; ap++)
+		v = (v << 8) | *(ap + 1);
+	return(v);
+}
