40cf6d2c27305300e8cfda13454d776f1caaf9d5
Batwan01 <wldhks1120@naver.com>
2023-11-14 11:04:57 +0900

    put_list

---
+++ assembly/put_list.c
@@ -0,0 +1,15 @@
+void put_lsit_head()
+{
+	printf(" LNO  Location  Label    Opcdoe    Operand       Object Code\n");
+	printf(" ---  --------  -----    ------    ----------    -----------\n");
+}
+
+void put_list()
+{
+	int i;
+	printf("%4s  %.7X  %-8s    %-8s    %-16s", LNO, LOC, LABEL, OPcode, OPerand), fflush(stdout);
+	for (i = 0; i < OBJC[0]; i++)
+		printf("%.2X", OBJC[i + 1]);
+	printf("\n");
+	OBJC[0] = 0;
+}
